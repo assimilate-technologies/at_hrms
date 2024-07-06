8c36e6f579fc7c2190cfe7e43cbe83c25a42e612
Mahesh Dev <86865338+maheshndev@users.noreply.github.com>
2024-07-06 08:13:51 +0530

    Update hooks.py

--- at_hrms/hooks.py
+++ at_hrms/hooks.py
@@ -10,7 +10,7 @@ app_license = "apache-2.0"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/at_hrms/css/theme_custom.css"
+# app_include_css = "/assets/at_hrms/css/theme_custom.css"
 # app_include_js = "/assets/at_hrms/js/at_hrms.js"
 
 # include js, css files in header of web template
@@ -229,14 +229,14 @@ app_include_css = "/assets/at_hrms/css/theme_custom.css"
 
 fixtures = [
 
-"Workflow State",
-"Workflow Action Master",
-{"doctype": "Workflow", "filters": []},
-{"doctype": "Notification", "filters": [[ "module", "=", "AT-HRMS" ]]},
-{"doctype": "Custom Field", "filters": [[ "module", "=", "AT-HRMS" ]]},
-{"doctype": "Role Profile", "filters": []},
-{"doctype": "Module Profile", "filters": []},
-{"doctype": "Workspace", "filters":[]},
+# "Workflow State",
+# "Workflow Action Master",
+# {"doctype": "Workflow", "filters": []},
+# {"doctype": "Notification", "filters": [[ "module", "=", "AT-HRMS" ]]},
+# {"doctype": "Custom Field", "filters": [[ "module", "=", "AT-HRMS" ]]},
+# {"doctype": "Role Profile", "filters": []},
+# {"doctype": "Module Profile", "filters": []},
+# {"doctype": "Workspace", "filters":[]},
 
 
 ]
